7c1bf47909cbc0f52686e4932a3a6c4d76e81fd1
Heungsub Lee <sub@subl.ee>
2015-12-21 20:26:05 +0900

    Respect PEP-8 coding style

--- base65536/__init__.py
+++ base65536/__init__.py
@@ -1,1 +1,12 @@
-from .core import encode, decode
+# -*- coding: utf-8 -*-
+"""
+   base65536
+   ~~~~~~~~~
+
+   Base-65536 Python implementation.
+
+"""
+from .core import decode, encode
+
+
+__all__ = ['encode', 'decode']
