3f63af1087af445c96bdd4e270c88f20c32b3c63
Heungsub Lee <sub@subl.ee>
2015-12-21 20:27:28 +0900

    Require six

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ test.run_tests = run_tests
 
 
 setup(
-    name="base65536",
-    version="0.1",
-    packages=["base65536"],
-    author="Ayun Park",
-    author_email="iamparkayun@gmail.com",
-    description="base65536",
-    keywords="base65536",
-    url="http://github.com/Parkayun/base65536",
+    name='base65536',
+    version='0.1',
+    packages=['base65536'],
+    author='Ayun Park',
+    author_email='iamparkayun@gmail.com',
+    description='base65536',
+    keywords='base65536',
+    url='http://github.com/Parkayun/base65536',
+    install_requires=['six'],
     tests_require=['pytest'],
 )
